8cf9dba1ad5bdff7af849324acae5053510cd856
fotis tsakiris <footios76@gmail.com>
2020-06-24 12:33:26 +0300

    user story 16 - 17

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { StyleSheet, useState } from "react";
+import React, { StyleSheet, useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [breakDecrement, setBreakDecrement] = useState(5);
-  const [breakIncrement, setBreakIncrement] = useState(5);
-  const [sessionDecrement, setSessionDecrement] = useState(25);
-  const [sessionIncrement, setSessionIncrement] = useState(25);
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+
+  useEffect(() => {
+console.log('breakLength', breakLength);
+
+    // Brake Length
+    if (breakLength  <= 0||  breakLength >= 5) {
+      setBreakLength(5);
+    }
+
+    // Session Length
+    if (sessionLength  <= 0||  sessionLength >= 25) {
+      setSessionLength(25);
+    }
+  }, [breakLength, setBreakLength, sessionLength, setSessionLength]);
 
   return (
     <div>
@@ -14,13 +26,21 @@ function App() {
         <div className="break-portion">
           <h3 id="break-label">Break Length</h3>
           <div style={styles.portionElements}>
-            <div onClick={() => setBreakDecrement(num => num - 1)} style={styles.decrement} id="break-decrement">
+            <div
+              onClick={() => setBreakLength(num => num - 1)}
+              style={styles.decrement}
+              id="break-decrement"
+            >
               -
             </div>
             <div style={styles.length} id="break-length">
-              {breakDecrement}
+              {breakLength}
             </div>
-            <div onClick={() => setBreakDecrement(num => num + 1)} style={styles.increment} id="break-increment">
+            <div
+              onClick={() => setBreakLength(num => num + 1)}
+              style={styles.increment}
+              id="break-increment"
+            >
               +
             </div>
           </div>
@@ -29,20 +49,30 @@ function App() {
         <div className="timer-portion">
           <h2 id="timer-label">Session</h2>
           <h1 id="time-left">25:00</h1>
-          <div id="start_stop">Start/Stop</div>
+          <div style={styles.sessionButtons} id="start_stop">
+            Start/Stop
+          </div>
           <div id="reset">Reset</div>
         </div>
         {/* ///////////////// */}
         <div className="session-portion">
           <h3 id="session-label">Session Length</h3>
           <div style={styles.portionElements}>
-            <div onClick={() => setSessionDecrement(num => num - 1)} style={styles.decrement} id="session-decrement">
+            <div
+              onClick={() => setSessionLength(num => num - 1)}
+              style={styles.decrement}
+              id="session-decrement"
+            >
               -
             </div>
             <div style={styles.length} id="session-length">
-              {sessionDecrement}
+              {sessionLength}
             </div>
-            <div onClick={() => setSessionDecrement(num => num + 1)} style={styles.increment} id="session-increment">
+            <div
+              onClick={() => setSessionLength(num => num + 1)}
+              style={styles.increment}
+              id="session-increment"
+            >
               +
             </div>
           </div>
@@ -69,15 +99,19 @@ const styles = (StyleSheet = {
     flexDirection: "row",
     alignItems: "flex-start",
     justifyContent: "space-around",
-    fontSize: 20
+    fontSize: 30
   },
   decrement: {
-    fontSize: 20
+    fontSize: 25
   },
   increment: {
-    fontSize: 20
+    fontSize: 25
   },
-  length: {}
+  length: {},
+  sessionButtons: {
+    margin: 20,
+    fontSize: 20
+  }
 });
 
 export default App;
